d33fb369ba95e20712d2382232e02640f9d8dc25
Saavedra Lucas <saavedralucas53@gmail.com>
2024-10-26 18:42:26 -0300

    TP8

--- estudiante/admin.py
+++ estudiante/admin.py
@@ -1,3 +1,13 @@
+# admin.py
 from django.contrib import admin
+from .models import Estudiante, Curso
 
-# Register your models here.
+@admin.register(Estudiante)
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido','curso')  
+    search_fields = ('nombre', 'apellido') 
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'cantidad_horas')  
+    search_fields = ('nombre', 'cantidad_horas')
